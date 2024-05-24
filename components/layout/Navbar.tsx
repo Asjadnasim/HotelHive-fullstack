688255a101d155002a53cc8ef8681b5a7f71dee5
Asjad Nasim <asjadnasim@gmail.com>
2024-05-24 20:29:15 +0530

    Clerk Using

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { UserButton } from '@clerk/nextjs';
+
+const Navbar = () => {
+	return (
+		<div>
+			<UserButton afterSignOutUrl='/' />
+		</div>
+	);
+};
+
+export default Navbar;
